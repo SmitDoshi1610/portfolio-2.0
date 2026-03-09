8c6f951ef3a334fa45912adb05c2e19ae3234314
SdTech1610 <sdtech1610@gmail.com>
2026-03-09 13:09:31 +0530

    remove semicolon

--- app/layout.tsx
+++ app/layout.tsx
@@ -195,7 +195,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-QES41G37S2"></script>
                 <script>
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+                function gtag(){dataLayer.push(arguments)}
                 gtag('js', new Date());
 
                 gtag('config', 'G-QES41G37S2');
