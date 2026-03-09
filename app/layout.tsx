46eb98915654d096d5508a456a50150e1701fcb0
SdTech1610 <sdtech1610@gmail.com>
2026-03-09 13:13:54 +0530

    add semicolon

--- app/layout.tsx
+++ app/layout.tsx
@@ -197,7 +197,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <Script id="google-analytics" strategy="afterInteractive">
                 {`
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments)}
+                function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', 'G-QES41G37S2');
